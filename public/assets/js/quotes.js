5f7d2a8ff172a4a1c705027058beae3393b83d3b
Adam Bilsky <adambilsky@gmail.com>
2018-09-03 15:16:54 -0500

    almost there

--- public/assets/js/quotes.js
+++ public/assets/js/quotes.js
@@ -1,7 +1,67 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
+var PORT = require("../server.js");
 
-  // jQuery event handlers should go here.
-// See day planner index.handlebars - send post requests as $.ajax calls type "POST"
-// $.ajax("/*whatever we call the route goes here/vice versa*")
-});
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+
+  // Capture the DELETE click
+  $(".delquote").on("click", function (event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request
+    $.ajax("/quotes/" + id, {
+      type: "DELETE"
+    }).then(
+      function () {
+        console.log("deleted id ", id);
+        //reload page
+        location.reload();
+      }
+    );
+  });
+  // Capture the ADD click
+  $(".create-form").on("submit", function (event) {
+    event.preventDefault();
+
+    var newQuote = {
+      author: $("#auth").val().trim(),
+      quote: $("#quo").val().trim()
+    };
+
+    // Send the POST request
+    $.ajax("/quotes", {
+      type: "POST",
+      data: newQuote
+    }).then(
+      function () {
+        console.log("Created a new quote!");
+        // reload page
+        location.reload();
+      });
+  });
+
+  // Capture the UPDATE click
+  $(".update-form").on("submit", function (event) {
+    event.preventDefault();
+
+    var updatedQuote = {
+      author: $("#auth").val().trim(),
+      quote: $("#quo").val().trim()
+    };
+
+    var id = $(this).data("id");
+
+
+    // Send the POST (Put) request
+    $.ajax("/quotes/" + id, {
+      type: "PUT",
+      data: updatedQuote
+    }).then(
+      function () {
+        console.log("Updated 1 quote!");
+        // reload page
+        location = "http://localhost:" + PORT;
+        location.reload();
+      });
+  });
+})
+
--- server.js
+++ server.js
@@ -2,7 +2,13 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = 3000;
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,7 +38,7 @@ connection.connect(function(err) {
 });
 
 // Express and MySQL code should go here.
-// Use Handlebars to render the main index.html page with the todos in it.
+// Use Handlebars to render the main index.html page with the quotes in it.
 app.get("/", function(req, res) {
   connection.query("SELECT * FROM quotes;", function(err, data) {
     if (err) {
@@ -43,15 +49,26 @@ app.get("/", function(req, res) {
   });
 });
 
+// Show individual quote and form to update
+app.get("/:id", function (req, res) {
+  connection.query("SELECT * FROM quotes where id = ?", [req.params.id], function (err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+    console.log(data);
+    res.render("single-quote", data[0]);
+  });
+});
+
 // POST NEW QUOTE ROUTE
 app.post("/quotes", function(req, res) {
-  connection.query("INSERT INTO quotes (quote, author) VALUES (?)", [req.body.quote], function(err, result) {
+  connection.query("INSERT INTO quotes (author, quote) VALUES (?, ?)", [req.body.author, req.body.quote], function(err, result) {
     if(err) {
       return res.status(500).end();
     }
     // return the ID of the new quote
     res.json({ id: result.insertId });
-    cosole.log({ id: result.insertId });
+    console.log({ id: result.insertId });
   });
 });
 
@@ -61,16 +78,46 @@ app.get("/quotes", function(req, res) {
     if (err) {
       return res.status(500).end();
     }
+    console.log(data);
     res.json(data);
-
+    
   })
 })
 // UPDATE QUOTES ROUTE
+app.put("/quotes/:id", function(req, res) {
+  connection.query("UPDATE quotes SET author = ?, quote = ? WHERE id = ?", [req.body.author, req.body.quote, req.params.id], 
+  function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.changedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  })
+})
 
 // DELETE QUOTE ROUTE
+app.delete("/quotes/:id", function(req, res) {
+  connection.query("DELETE FROM quotes WHERE id = ?", [req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  });
+});
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
+module.exports = PORT;
